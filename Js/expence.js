36f4d09366c1043a712b265442d211709b2df519
abujayed007 <abujayed007@gmail.com>
2022-08-23 15:16:26 +0600

    completed budget

---
+++ Js/expence.js
@@ -0,0 +1,39 @@
+document.getElementById('btn-calculate').addEventListener('click', function(){
+    const playerField = document.getElementById('player-field')
+    const newPlayerFieldString = playerField.value;
+    const newExpencesAmount = parseFloat(newPlayerFieldString);
+    
+    playerField.value = '';
+
+    const playerExpences = document.getElementById('player-expence')
+    const newPlayerExpence = playerExpences.innerText;
+    playerExpences.innerText =newExpencesAmount;
+
+})
+document.getElementById('total-btn').addEventListener('click', function(){
+    const managerField = document.getElementById('manager-field');
+    const managerExpenceString = managerField.value;
+    const managerExpence = parseFloat(managerExpenceString);
+    managerField.value = '';
+
+    const coachField = document.getElementById('coach-field');
+    const coachExpenceString = coachField.value;
+    const coachExpence = parseFloat(coachExpenceString);
+
+    coachField.value= '';
+
+    const playerExpences = document.getElementById('player-expence')
+    const newPlayerExpenceString = playerExpences.innerText;
+    const newPlayerExpence = parseFloat(newPlayerExpenceString);
+   
+
+
+    const teamTotalExpences = newPlayerExpence + coachExpence + managerExpence;
+
+
+    const totalExpences = document.getElementById('total-expence')
+    totalExpences.innerText = teamTotalExpences;
+
+
+    
+})
---
+++ Js/player.js
@@ -0,0 +1,15 @@
+const allPlayers = [];
+function displayView(playerNumber){
+    
+}
+function playerSelect(players)
+{
+    const playersName = players.parentNode.parentNode.children[0].innerText;
+
+    const playerObj = {
+        playersName
+    }
+    // console.log(playerObj)
+    allPlayers.push(playerObj)
+    // console.log(playerObj)
+}
