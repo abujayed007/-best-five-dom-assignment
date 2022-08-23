d46bf9019e72e998067abfcc98a15cf39de5f070
abujayed007 <abujayed007@gmail.com>
2022-08-23 17:49:01 +0600

    completed selected section

--- Js/player.js
+++ Js/player.js
@@ -1,15 +1,24 @@
 const allPlayers = [];
-function displayView(playerNumber){
+function topPlayers(){
+    const playerSelect = document.getElementById('selected-item');
+    playerSelect.innerText = '';
+
+    for(let i = 0; i < allPlayers.length; i++){
+        const playerList = allPlayers[i];
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+        <th>${i+1}.</th>
+        <th>${playerList}</th>
+        `
+        playerSelect.appendChild(tr);
     
+    }
 }
+
 function playerSelect(players)
 {
     const playersName = players.parentNode.parentNode.children[0].innerText;
-
-    const playerObj = {
-        playersName
-    }
-    // console.log(playerObj)
-    allPlayers.push(playerObj)
-    // console.log(playerObj)
+    allPlayers.push(playersName)
+    // console.log(allPlayers)
+    topPlayers(allPlayers);
 }
